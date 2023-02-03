eef968feda88de7584ce082006cd62977016d9a8
Karina Vargas <karina.vargas@jala.university>
2023-02-03 10:57:05 -0500

    webpack demo in class

---
+++ webpack-demo/webpack.dev.js
@@ -0,0 +1,14 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const path = require("path");
+
+module.exports = merge(common, {
+    mode: 'development',
+    entry: {
+        main: './src/index.js',
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'dev.js'
+    },
+});
